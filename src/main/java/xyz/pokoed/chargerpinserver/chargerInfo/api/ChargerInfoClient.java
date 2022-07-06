26c734feadf38ebed8ecd7ec333e4054d70eda78
박종연 <20041230andy@gmail.com>
2022-07-06 21:44:33 +0900

    Update ChargerInfoClient.java
    Client의 필요없는 모듈의 import를 제거

--- src/main/java/xyz/pokoed/chargerpinserver/chargerInfo/api/ChargerInfoClient.java
+++ src/main/java/xyz/pokoed/chargerpinserver/chargerInfo/api/ChargerInfoClient.java
@@ -14,7 +14,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLDecoder;
