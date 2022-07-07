f352d2a8fa3068523e0ea896a8ea1d3c01d54dbb
박종연 <20041230andy@gmail.com>
2022-07-08 01:07:13 +0900

    Update UserBookmarkChargerRepository.java

--- src/main/java/xyz/pokoed/chargerpinserver/repository/UserBookmarkChargerRepository.java
+++ src/main/java/xyz/pokoed/chargerpinserver/repository/UserBookmarkChargerRepository.java
@@ -6,10 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 import xyz.pokoed.chargerpinserver.model.UserBookmarkChargerEntity;
 
+import java.util.List;
+
 public interface UserBookmarkChargerRepository extends JpaRepository<UserBookmarkChargerEntity, Long> {
     @Transactional
     @Modifying
     @Query("delete from UserBookmarkChargerEntity u where u.userId = ?1")
     void deleteByUserId(String userId);
 
+    @Transactional
+    @Modifying
+    @Query("select u from UserBookmarkChargerEntity u where u.userId = ?1")
+    List<UserBookmarkChargerEntity> findByUserId(String userId);
 }
