dbd1f2feda449426ed2afdcc4461b0c06461bdc3
박종연 <20041230andy@gmail.com>
2022-07-07 20:09:34 +0900

    Update ChargerInfoResponse.java

--- src/main/java/xyz/pokoed/chargerpinserver/chargerInfo/model/ChargerInfoResponse.java
+++ src/main/java/xyz/pokoed/chargerpinserver/chargerInfo/model/ChargerInfoResponse.java
@@ -11,41 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChargerInfoResponse {
-    private Header header;
-    private Body body;
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public class Header {
-        private String resultCode;
-        private String resultMsg;
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public class Body {
-        private List<Item> items;
-        private int numOfRows;
-        private int pageNo;
-        private int totalCount;
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public class Item {
-        private String addr;
-        private int chargeTp;
-        private int cpId;
-        private String cpNm;
-        private int cpStat;
-        private int cpTp;
-        private int csId;
-        private int csNm;
-        private int lat;
-        private int longi;
-        private Timestamp statUpdateDatetime;
-    }
+    private int code;
+    private String msg;
 }
+
