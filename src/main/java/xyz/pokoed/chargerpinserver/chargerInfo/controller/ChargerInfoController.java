4a7a21cdb0a48d13e37d440d6fe81230c9347e62
박종연 <20041230andy@gmail.com>
2022-07-07 20:09:21 +0900

    Update ChargerInfoController.java

--- src/main/java/xyz/pokoed/chargerpinserver/chargerInfo/controller/ChargerInfoController.java
+++ src/main/java/xyz/pokoed/chargerpinserver/chargerInfo/controller/ChargerInfoController.java
@@ -2,13 +2,10 @@ package xyz.pokoed.chargerpinserver.chargerInfo.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import xyz.pokoed.chargerpinserver.chargerInfo.api.ChargerInfoClient;
+import xyz.pokoed.chargerpinserver.chargerInfo.model.ChargerInfoRequest;
 import xyz.pokoed.chargerpinserver.chargerInfo.service.ChargerInfoServiceImpl;
-import xyz.pokoed.chargerpinserver.model.UserEntity;
 
 @Slf4j
 @CrossOrigin
@@ -20,11 +17,12 @@ public class ChargerInfoController {
     private ChargerInfoClient chargerInfoClient;
 
     @PostMapping("/info")
-    public Object searchCharger() {
+    public Object searchCharger(@RequestBody ChargerInfoRequest request) {
         log.debug("/info start");
+        
+//        log.info("{}", request.getKeyword());
 
-
-//        log.info("return {}", );
-        return chargerInfoClient.getChargerInfo("");
+        // 위치, 이름 모두 됨
+        return chargerInfoService.searchInfo(request.getKeyword());
     }
 }
