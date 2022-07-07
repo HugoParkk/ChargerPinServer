7ddb1e2e3ef542348b78f131f6b52333b7f1f3dc
박종연 <20041230andy@gmail.com>
2022-07-08 01:07:04 +0900

    Create BookmarkController.java

---
+++ src/main/java/xyz/pokoed/chargerpinserver/bookmark/controller/BookmarkController.java
@@ -0,0 +1,15 @@
+package xyz.pokoed.chargerpinserver.bookmark.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@CrossOrigin
+@AllArgsConstructor
+@Controller
+@RequestMapping("/bookmark")
+public class BookmarkController {
+}
