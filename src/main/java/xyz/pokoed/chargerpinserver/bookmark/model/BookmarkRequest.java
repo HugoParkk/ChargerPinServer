aec13f6211716cbc99f10d0f052db8879479a65d
박종연 <20041230andy@gmail.com>
2022-07-08 01:06:56 +0900

    Created Bookmark Req/Res Models

---
+++ src/main/java/xyz/pokoed/chargerpinserver/bookmark/model/BookmarkRequest.java
@@ -0,0 +1,12 @@
+package xyz.pokoed.chargerpinserver.bookmark.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookmarkRequest {
+
+}
---
+++ src/main/java/xyz/pokoed/chargerpinserver/bookmark/model/BookmarkResponse.java
@@ -0,0 +1,11 @@
+package xyz.pokoed.chargerpinserver.bookmark.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookmarkResponse {
+}
