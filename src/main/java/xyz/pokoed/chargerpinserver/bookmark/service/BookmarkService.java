27cc3338de61225a711b3893f1979b057d8be530
박종연 <20041230andy@gmail.com>
2022-07-10 22:41:14 +0900

    updated BookmarkServices

--- src/main/java/xyz/pokoed/chargerpinserver/bookmark/service/BookmarkService.java
+++ src/main/java/xyz/pokoed/chargerpinserver/bookmark/service/BookmarkService.java
@@ -1,4 +1,10 @@
 package xyz.pokoed.chargerpinserver.bookmark.service;
 
+import org.springframework.http.ResponseEntity;
+import xyz.pokoed.chargerpinserver.bookmark.model.BookmarkRequest;
+import xyz.pokoed.chargerpinserver.bookmark.model.BookmarkResponse;
+
 public interface BookmarkService {
+    public ResponseEntity<BookmarkResponse> add(BookmarkRequest request);
+    public ResponseEntity<BookmarkResponse> delete(BookmarkRequest request);
 }
--- src/main/java/xyz/pokoed/chargerpinserver/bookmark/service/BookmarkServiceImpl.java
+++ src/main/java/xyz/pokoed/chargerpinserver/bookmark/service/BookmarkServiceImpl.java
@@ -1,10 +1,23 @@
 package xyz.pokoed.chargerpinserver.bookmark.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import xyz.pokoed.chargerpinserver.bookmark.model.BookmarkRequest;
+import xyz.pokoed.chargerpinserver.bookmark.model.BookmarkResponse;
 
 @Service
 @AllArgsConstructor
 public class BookmarkServiceImpl implements BookmarkService {
 
+    @Override
+    public ResponseEntity<BookmarkResponse> add(BookmarkRequest request) {
+
+        return ResponseEntity.ok(new BookmarkResponse());
+    }
+
+    @Override
+    public ResponseEntity<BookmarkResponse> delete(BookmarkRequest request) {
+        return ResponseEntity.ok(new BookmarkResponse());
+    }
 }
