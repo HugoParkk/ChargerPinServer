45c5447c413b037e2700d6f08ab616eec43bd585
박종연 <20041230andy@gmail.com>
2022-07-11 01:04:25 +0900

    Update BookmarkController.java

--- src/main/java/xyz/pokoed/chargerpinserver/bookmark/controller/BookmarkController.java
+++ src/main/java/xyz/pokoed/chargerpinserver/bookmark/controller/BookmarkController.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import xyz.pokoed.chargerpinserver.bookmark.model.BookmarkRequest;
+import xyz.pokoed.chargerpinserver.bookmark.service.BookmarkServiceImpl;
 
 @Slf4j
 @CrossOrigin
@@ -12,4 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/bookmark")
 public class BookmarkController {
+    private final BookmarkServiceImpl bookmarkService;
+
+    @PostMapping("/add")
+    public Object addBookmark(@RequestBody BookmarkRequest request) {
+        log.info("/bookmark/add start");
+
+        return bookmarkService.add(request);
+    }
+
+    @PostMapping("/delete")
+    public Object deleteBookmark(@RequestBody BookmarkRequest request) {
+        log.info("/bookmark/delete start");
+
+        return bookmarkService.delete(request);
+    }
 }
