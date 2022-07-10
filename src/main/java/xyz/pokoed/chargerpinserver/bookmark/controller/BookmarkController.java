1d58fc9d61b08ff387345d03dbe9678fa3ff2328
박종연 <20041230andy@gmail.com>
2022-07-11 08:35:28 +0900

    Update BookmarkController.java

--- src/main/java/xyz/pokoed/chargerpinserver/bookmark/controller/BookmarkController.java
+++ src/main/java/xyz/pokoed/chargerpinserver/bookmark/controller/BookmarkController.java
@@ -3,10 +3,7 @@ package xyz.pokoed.chargerpinserver.bookmark.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import xyz.pokoed.chargerpinserver.bookmark.model.BookmarkRequest;
 import xyz.pokoed.chargerpinserver.bookmark.service.BookmarkServiceImpl;
 
@@ -25,10 +22,24 @@ public class BookmarkController {
         return bookmarkService.add(request);
     }
 
-    @PostMapping("/delete")
-    public Object deleteBookmark(@RequestBody BookmarkRequest request) {
+    @GetMapping("/read/{userId}")
+    public Object readBookmark(@PathVariable String userId) {
+        log.info("/bookmark/read start");
+
+        return bookmarkService.read(userId);
+    }
+
+    @PatchMapping("/update")
+    public Object updateBookmark(@RequestBody BookmarkRequest request) {
+        log.info("/bookmark/update start");
+
+        return bookmarkService.update(request);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Object deleteBookmark(@PathVariable Long id) {
         log.info("/bookmark/delete start");
 
-        return bookmarkService.delete(request);
+        return bookmarkService.delete(id);
     }
 }
