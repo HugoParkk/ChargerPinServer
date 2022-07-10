4a83a30aaed799a2104228d9b55e601e5f8f8837
박종연 <20041230andy@gmail.com>
2022-07-11 01:05:06 +0900

    Updated BookmarkServices

--- src/main/java/xyz/pokoed/chargerpinserver/bookmark/service/BookmarkService.java
+++ src/main/java/xyz/pokoed/chargerpinserver/bookmark/service/BookmarkService.java
@@ -4,7 +4,10 @@ import org.springframework.http.ResponseEntity;
 import xyz.pokoed.chargerpinserver.bookmark.model.BookmarkRequest;
 import xyz.pokoed.chargerpinserver.bookmark.model.BookmarkResponse;
 
+import java.util.List;
+
 public interface BookmarkService {
     public ResponseEntity<BookmarkResponse> add(BookmarkRequest request);
+    public List<ResponseEntity<BookmarkResponse>> read(String userId);
     public ResponseEntity<BookmarkResponse> delete(BookmarkRequest request);
 }
--- src/main/java/xyz/pokoed/chargerpinserver/bookmark/service/BookmarkServiceImpl.java
+++ src/main/java/xyz/pokoed/chargerpinserver/bookmark/service/BookmarkServiceImpl.java
@@ -5,19 +5,42 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import xyz.pokoed.chargerpinserver.bookmark.model.BookmarkRequest;
 import xyz.pokoed.chargerpinserver.bookmark.model.BookmarkResponse;
+import xyz.pokoed.chargerpinserver.model.UserBookmarkChargerEntity;
+import xyz.pokoed.chargerpinserver.repository.UserBookmarkChargerRepository;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class BookmarkServiceImpl implements BookmarkService {
+    private UserBookmarkChargerRepository userBookmarkChargerRepository;
 
     @Override
     public ResponseEntity<BookmarkResponse> add(BookmarkRequest request) {
+        if (userBookmarkChargerRepository.existsByChargerId(request.getChargerId())) {
+            return ResponseEntity.badRequest().body(new BookmarkResponse(0, "존재하는 아이디입니다."));
+        }
+        UserBookmarkChargerEntity bookmarkChargerEntity = UserBookmarkChargerEntity.builder()
+                .chargerName(request.getChargerName())
+                .chargerId(request.getChargerId())
+                .userId(request.getUserId())
+                .build();
+        userBookmarkChargerRepository.save(bookmarkChargerEntity);
+        return ResponseEntity.ok(new BookmarkResponse(1, "ok"));
+    }
 
-        return ResponseEntity.ok(new BookmarkResponse());
+    @Override
+    public List<ResponseEntity<BookmarkResponse>> read(String userId) {
+        List<UserBookmarkChargerEntity> bookmarkList = userBookmarkChargerRepository.findByUserId(userId);
+        bookmarkList.stream().forEach(e -> {
+            System.out.println(e.getChargerName());
+        });
+        return null;
     }
 
     @Override
     public ResponseEntity<BookmarkResponse> delete(BookmarkRequest request) {
-        return ResponseEntity.ok(new BookmarkResponse());
+        userBookmarkChargerRepository.deleteById(request.getBookmarkId());
+        return ResponseEntity.ok(new BookmarkResponse(1, "ok"));
     }
 }
