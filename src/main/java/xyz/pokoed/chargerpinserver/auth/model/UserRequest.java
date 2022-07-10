4c5f706980880d6247c2d42c8bfa5a48ab2d0579
박종연 <20041230andy@gmail.com>
2022-07-10 22:40:33 +0900

    Update UserRequest.java

--- src/main/java/xyz/pokoed/chargerpinserver/auth/model/UserRequest.java
+++ src/main/java/xyz/pokoed/chargerpinserver/auth/model/UserRequest.java
@@ -1,6 +1,8 @@
 package xyz.pokoed.chargerpinserver.auth.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
