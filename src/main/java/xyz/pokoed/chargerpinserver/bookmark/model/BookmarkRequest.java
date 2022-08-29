a3746d2624d512e9642668c7d57ef66223b2d6d2
박종연 <20041230andy@gmail.com>
2022-08-30 02:47:33 +0900

    Update BookmarkRequest.java

--- src/main/java/xyz/pokoed/chargerpinserver/bookmark/model/BookmarkRequest.java
+++ src/main/java/xyz/pokoed/chargerpinserver/bookmark/model/BookmarkRequest.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 public class BookmarkRequest {
     private Long bookmarkId;
     private String chargerName;
+    private String chargerAddr;
     private int chargerId;
     private String userId;
 }
